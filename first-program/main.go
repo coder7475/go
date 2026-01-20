8894cfbbef836d276bdfe8d4df7f785907bb6938
coder7475 <robiulhossain7475@gmail.com>
2026-01-20 21:40:03 +0600

    feat: variables and data types

--- first-program/main.go
+++ first-program/main.go
@@ -3,5 +3,51 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, world!");
+  // fmt.Println("Hello, world!");
+	
+
+	//a := 10
+
+	//fmt.Println(a);
+	/**
+	* int 
+	* float32
+	* bool 
+	* string
+	**/
+	// declare a variable
+	// interger
+  //	var x int = 10;
+
+	// fmt.Println(x);
+
+	// without type
+  //	var a = 10;
+
+	// fmt.Println(a);
+
+	// without var keyword and type
+	// m := 10;
+	ch := 'H'
+
+	fmt.Println(ch); // 72
+	
+	s := "Hello! World!";
+	fmt.Println(s) // Hello! World!
+
+	// bool
+	t := true;
+	// ressagnment
+	t = false;
+	// can't assign another type
+	//t = 'jlalsdf';
+
+	fmt.Println(t); // false
+
+	// constant
+	const c = 100;
+
+	// can't ressagnment
+	//c = 300;
+	fmt.Println(c); // 100
 }
