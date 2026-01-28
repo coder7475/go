bd52274b07538fa9296e85b55fd0bf33d3568f92
coder7475 <robiulhossain7475@gmail.com>
2026-01-28 21:14:46 +0600

    feat: refactor function

--- examples/input.go
+++ examples/input.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func main()  {
+func sum(var num1 int, var num2 int) int {
+	sum := num1 + num2
+
+	return sum
+}
+
+func getUserName() string {
 	fmt.Println("Enter your name: ")
 	
 	var name string
 	fmt.Scanln(&name)
 
-	var num1 int 
-	var num2 int 
+	return name
+}
+
+
+func main()  {
+	name := getUserName()
+
+	var num1 int
+	var num2 int
 
 	fmt.Println("Enter first number - ")
 	fmt.Scanln(&num1)
@@ -17,9 +30,8 @@ func main()  {
 	fmt.Println("Enter second number - ")
 	fmt.Scanln(&num2)
 
-	sum := num1 + num2
+	sum := sum(num1, num2)
 
 	fmt.Println("Hello, ", name)
 	fmt.Println("Summation: ", sum)
-
 }
